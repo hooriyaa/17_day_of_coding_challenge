6abd3528c3d91bf6107dd7795c1aa6fd0ce584e5
hooriya <parih3080@gmail.com>
2024-04-22 22:25:28 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,34 @@
+// QUESTION : 49
+// FUNCTION WITH REST PARAMETER:
+
+function myHobbies (...hobbies:string[]){
+//   creat for loop to excess each hobby
+for( let i=0; i<hobbies.length; i++){
+console.log(`I Enjoy ${hobbies} `)
+}
+}
+// Call the function:
+myHobbies ("Painting");
+myHobbies("reading");
+myHobbies("stam collecting");
+myHobbies("Gardening")
+
+
+// QUESTION 50:
+let myIdealDay=`My ideal day includes in following:
+1. I wakeup naturally with morning light I feel refreshed.
+I have a glass of water and then enjoy a morning walk.
+
+2. Then Enjoy picnic with my Friends Having a lots o fun.
+
+3. at the end I read a grat book that give me a productive ideas.`;
+console.log(myIdealDay);
+
+
+// QUESTION 51:
+function calculatearea( height:number, width:number){
+    return height*width
+}
+let refactorArea=(width:number,height:number)=>height*width
+
+console.log(refactorArea(4,9))
